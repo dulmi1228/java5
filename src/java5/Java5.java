7d3962a22cb352fba91d78d31d439726b06cb044
dulmi1228 <48253729+dulmi1228@users.noreply.github.com>
2019-04-14 16:02:36 +0530

    java5
    Java Program to check Even or Odd number

---
+++ src/java5/Java5.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Dulmi
+ */
+public class Java5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int num;
+    System.out.println("Enter an Integer number:");
+
+    //The input provided by user is stored in num
+    Scanner input = new Scanner(System.in);
+    num = input.nextInt();
+
+    /* If number is divisible by 2 then it's an even number
+     * else odd number*/
+    if ( num % 2 == 0 )
+        System.out.println("Entered number is even");
+     else
+        System.out.println("Entered number is odd");
+        // TODO code application logic here
+    }
+    
+}
